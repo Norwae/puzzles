a34371a7094c9f5fc7384769cb6fe1b73e4e407e
Terra <terra@spacetime.de>
2016-05-27 21:02:00 +0200

    Cut down to the task

---
+++ src/main/scala/com/github/norwae/AmazingSyntax.scala
@@ -0,0 +1,14 @@
+package com.github.norwae
+
+case class AmazingSyntax(private var _name: String, var values: List[String]) {
+  // your code here
+}
+
+case class Left(value: Int) {
+  def +(other: Left) = {
+    // your code here
+  }
+}
+object Right {
+  // your code here
+}
